b41984b6afd93f9a73acf7540789cadd38e88bd6
kbaybekov <k.baybekov@gmail.com>
2026-01-14 12:10:09 +0000

    renaming of test scripts

--- tests/e2e/modules/scheduler/__init__.py
+++
@@ -1,1 +0,0 @@
-from modules.filesystem.metas import SampleMeta, ResultMeta
--- tests/integration/modules/scheduler/classes/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pipeline import Pipeline
-from .task_slurm_job import TaskSlurmJob
-from .task_data import TaskData
